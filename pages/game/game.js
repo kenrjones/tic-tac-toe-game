d3a8548222eb2becb4c669dde0ef1ef3f5dbdd78
kenrjones <krjones502@gmail.com>
2019-10-15 10:40:56 -0400

    added more visual elemnts to css and wrote basic pseudo code for js logic

--- pages/game/game.js
+++ pages/game/game.js
@@ -1,1 +1,42 @@
 console.log('game.js loaded');
+
+
+// // Technical Requirements
+//     Render a game board in the browser
+//     Switch turns between X and O (or whichever markers you select)
+//     Visually display which side won if a player gets three in a row, or show a draw if neither player wins
+//     Include separate HTML / CSS / JavaScript files
+//     Stick with KISS (Keep It Simple Stupid) and DRY (Don't Repeat Yourself) principles
+//     Use JavaScript for DOM manipulation
+//     D  eploy your game online on GitHub Pages, where the rest of the world can access it
+//     We will be walking you through this process!
+//     Use semantic markup for HTML and CSS (adhere to best practices)
+//     Have well-formatted, and well-commented code (e.g. Use Prettier and follow these HTML & CSS Guidelines)
+//     Some unique features that distinguish your game from the "normal" solution listed above. Here are some ideas:
+
+
+
+// the grid boxes need to return an 'X' or 'O' when clicked.
+
+
+// a display is need to show which players turn it is to make a selecton.
+
+
+// store the seclections of each player.
+
+
+// collect all of the possible winning combinations.
+
+
+// alert when there is a winner or a draw.
+
+
+// store and display the points/number of gmaes won for each player.
+
+
+// clear the board after each game.
+
+
+// set a button to refesh page and restart game to zero played and zero points.
+
+
